334c2ddf0c1a2478fa0e761eeb53239d8079d33f
Erikka Amrine <erikkaamrine@hotmail.com>
2022-02-25 00:02:00 -0500

    js file updated

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log (playerName);
+console.log("this log's a string, good for leaving yourself a message");
+console.log(10+10);
+console.log("My robot's name is"+ playerName);
+//this creates a function named fight
+function fight() {
+    window.alert("The fight has begun!");
+}
+//fight();
