773a27ee8d38f7f07385ccefe47f0846f00a65b0
Erikka Amrine <erikkaamrine@hotmail.com>
2022-02-25 12:23:51 -0500

    round one structure complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,35 @@
 var playerName = window.prompt("What is your robot's name?");
-console.log (playerName);
-console.log("this log's a string, good for leaving yourself a message");
-console.log(10+10);
-console.log("My robot's name is"+ playerName);
-//this creates a function named fight
-function fight() {
-    window.alert("The fight has begun!");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+};
+
+fight();
+enemyHealth= enemyHealth-playerAttack;
+console.log(playerName + "attacked" + enemyName +"." + enemyName + "now has" + enemyHealth + "health remaining.")
+
+if (enemyHealth <= 0) {
+    window.alert (enemyName+ "has died!");
 }
-//fight();
+else {
+    window.alert (enemyName +"still has"+ enemyHealth+ "health left.");
+}
+
+playerHealth= playerHealth-enemyAttack;
+console.log(enemyName + "attacked"+ playerName+ "."+ playerName+ "now has"+ playerHealth+ "health remaining.");
+
+if (playerHealth <=0) {
+    window.alert(playerName+ "has died!");
+}
+else {
+    window.alert(playerName+ "still has"+ playerHealth+ "health left.");
+}
